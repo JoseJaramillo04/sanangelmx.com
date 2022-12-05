e702c130d7616a6a29def7efe7035a48422451e9
Fernando Jaramillo <jose.jaramillo04@utrgv.edu>
2022-12-04 23:30:34 -0600

    finished section title structure

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min"; //Boostrap Import 2/2
 import "./homePageStyle.css";
 import Header from "./components/Header";
 import Carousel from "./components/Carousel";
+import SectionTitle from "./components/SectionTitle";
 
 function Home() {
   const navigate = useNavigate();
@@ -21,6 +22,9 @@ function Home() {
   return (
     <>
       <Carousel />
+      <div className="content">
+        <SectionTitle />
+      </div>
     </>
   );
 }
---
+++ src/components/SectionTitle.tsx
@@ -0,0 +1,12 @@
+import "../css/sectionTitle.css";
+
+function SectionTitle() {
+  return (
+    <div className="section-container">
+      <span className="section-title">Popular Categories</span>
+      <hr className="section-line" />
+    </div>
+  );
+}
+
+export default SectionTitle;
